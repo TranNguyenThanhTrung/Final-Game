b24a071780950e87be999b97fb366a26aec0a742
Trung910 <184208549+TranNguyenThanhTrung@users.noreply.github.com>
2024-10-17 21:26:45 +0700

    BreakOut(ốm làm đc tí)

--- Assets/FinalGame/Scripts/AiBehavio/Custommer/CustommerBehavior.cs
+++ Assets/FinalGame/Scripts/AiBehavio/Custommer/CustommerBehavior.cs
@@ -13,6 +13,11 @@ public class CustommerBehavior : MonoBehaviour
         Apple,
         None
     }
+    public enum ActionState
+    {
+        Idle,
+        Working
+    }
 
     private NavMeshAgent agent;
     private BehaviorTree tree;
@@ -20,19 +25,23 @@ public class CustommerBehavior : MonoBehaviour
     private Node.NodeState treeStatus = Node.NodeState.RUNNING;
     public ActionState state = ActionState.Idle;
 
-    public enum ActionState
-    {
-        Idle,
-        Working
-    }
+
+    public bool isEmtyChair;
+    public bool isServed;
+
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
+    private void Start()
+    {
+        Node customerBTRoot = InitializeBehaviorTree();
+        tree = new BehaviorTree(customerBTRoot);
+    }
 
 
-    private void InitializeBehaviorTree()
+    private Node InitializeBehaviorTree()
     {
         Leaf processPayment = new Leaf(Pay);
         Leaf leaveRestaurant = new Leaf(LeaveRestaurant);
@@ -40,40 +49,61 @@ public class CustommerBehavior : MonoBehaviour
         Leaf goToEmptyChair = new Leaf(GoToEmptyChair);
         Leaf goToFrontDoor = new Leaf(GoToFrontDoor);
 
-        Sequence hasBeenServed = new Sequence(new List<Node> { processPayment, leaveRestaurant});
-        Sequence notBeenServed = new Sequence(new List<Node> { isWaiting});
+        Sequence hasBeenServed = new Sequence(new List<Node> { processPayment, leaveRestaurant });
+        Sequence notBeenServed = new Sequence(new List<Node> { isWaiting });
         Selector orderCheck = new Selector(new List<Node> { hasBeenServed, notBeenServed });
-        Sequence hasChair = new Sequence(new List<Node>  {goToEmptyChair, orderCheck });
-        Sequence noAvailableChair = new Sequence(new List<Node> { leaveRestaurant});
-        Selector checkSeatAvailability = new Selector(new List<Node> { hasChair, noAvailableChair});
+        Sequence hasChair = new Sequence(new List<Node> { goToEmptyChair, orderCheck });
+        Sequence noAvailableChair = new Sequence(new List<Node> { leaveRestaurant });
+        Selector checkSeatAvailability = new Selector(new List<Node> { hasChair, noAvailableChair });
 
-        Sequence enterRestaurant = new Sequence(new List<Node> {goToFrontDoor , checkSeatAvailability });
+        Sequence enterRestaurant = new Sequence(new List<Node> { goToFrontDoor, checkSeatAvailability });
+        return enterRestaurant;
+    }
+    private void Update()
+    {
+        tree.Update();
     }
 
     private Node.NodeState GoToFrontDoor()
     {
-        throw new NotImplementedException();
+        Debug.Log("Dang di toi cua chinh");
+        return Node.NodeState.SUCCESS;
     }
 
     private Node.NodeState GoToEmptyChair()
     {
-        throw new NotImplementedException();
+        if (isEmtyChair == true)
+        {
+            Debug.Log("Dang tien toi ghe");
+            return Node.NodeState.SUCCESS;
+        }
+        else
+        {
+            Debug.Log(" Khong co ghe trong");
+            return Node.NodeState.FAILURE;
+        }
     }
 
     private Node.NodeState IsWaiting()
     {
-        throw new NotImplementedException();
+        Debug.Log("Dang cho mon");
+        return Node.NodeState.SUCCESS;
     }
 
     private Node.NodeState LeaveRestaurant()
     {
-        throw new NotImplementedException();
+        Debug.Log("Roi nha hang");
+        return Node.NodeState.SUCCESS;
     }
 
     private Node.NodeState Pay()
-    { 
-    
-        return Node.NodeState.SUCCESS;
+    {
+        if (isServed == true)
+        {
+            Debug.Log("Dang tinh tien");
+            return Node.NodeState.SUCCESS;
+        }
+        else { return Node.NodeState.FAILURE; }
     }
     private Node.NodeState GoToLocation(Vector3 destination)
     {
